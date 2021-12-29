b7187b50fbc6195535eb46089e0ae9475b3333e3
Inna <shvartz_i@mail.ru>
2021-12-30 01:43:44 +0300

    Task24 added

---
+++ Task24_Stepen3/Program.cs
@@ -0,0 +1,6 @@
+﻿// найти кубы чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+for (int i=1; i <= N; i++)
+   Console.WriteLine ($"{i}  {i*i*i} ");
