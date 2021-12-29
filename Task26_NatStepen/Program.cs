d90fe5ae127fd44d4330d03b3e8552dffbe849ae
Inna <shvartz_i@mail.ru>
2021-12-30 02:28:05 +0300

    Task26 added

---
+++ Task26_NatStepen/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.Write ("Введите степень: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+int Result = 1;
+int index = 0;
+while (index<B)
+    {
+        Result = Result*A;
+        index++;
+    }
+// for (int index = 1; index <= B; index++)
+//    Result = Result*A;
+
+Console.WriteLine ($"{A} в степени {B} равно {Result}");
