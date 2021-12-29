98caad592c5218e6cc3c291db5afd4428c3fa891
Inna <shvartz_i@mail.ru>
2021-12-30 01:54:45 +0300

    Task 25 added

---
+++ Task25_Summ/Program.cs
@@ -0,0 +1,7 @@
+﻿// Сумма чисел от 1 до А
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+int Sum = 0;
+for (int i=1; i <= A; i++)
+    Sum = Sum + i;
+Console.WriteLine($"Сумма чисел от 1 до {A} равна {Sum}");
