32f7b244e756ffbb35f4ec9ab1222ba4ac57a5f5
Inna <shvartz_i@mail.ru>
2021-12-29 23:39:48 +0300

    Тask23 added

---
+++ Task23_TableSquare/Program.cs
@@ -0,0 +1,5 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+for (int i=1; i <= N; i++)
+   Console.WriteLine ($"{i}  {i*i} ");
