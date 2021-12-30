64abda98dcb9cdf9735ce07023723ecc8b912e56
Inna <shvartz_i@mail.ru>
2021-12-30 03:24:07 +0300

    Task 27 and 28 added

---
+++ Task27_28_SumKolDigits/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить количество цифр в числе (27) и посчитать сумму цифр чила (28)
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+if (A < 0) A = -A;
+int TempA = A;
+int Sum = 0;
+int Num = 0;
+do 
+    {
+        Sum = Sum + TempA % 10;
+        Num++;
+        TempA = TempA / 10;
+    }
+while (TempA!=0);
+
+Console.WriteLine($"Количество цифр в числе {A} - {Num}, их сумма равна {Sum}");
