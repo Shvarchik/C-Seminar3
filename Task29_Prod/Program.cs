e32fc1d687206440bb0c788631cc095d3d344710
Inna <shvartz_i@mail.ru>
2021-12-30 03:37:15 +0300

    Task 29 added

---
+++ Task29_Prod/Program.cs
@@ -0,0 +1,9 @@
+﻿// Произведение чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int Prod = 1;
+for (int i=2; i <= N; i++)
+    Prod = Prod*i;
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {Prod}");
+
