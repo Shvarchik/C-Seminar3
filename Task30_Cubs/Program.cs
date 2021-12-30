bfc328109bcf5b4db768187ddf183df40d0ab332
Inna <shvartz_i@mail.ru>
2021-12-30 03:50:33 +0300

    Task 30 added

---
+++ Task30_Cubs/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+if (N % 2 == 0)
+    Console.WriteLine ($"{N} в кубе равно {N*N*N}");
+else
+    Console.WriteLine($"{N} заканчивается на нечетную цифру");
